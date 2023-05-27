60699f4558f67cbf5be97579e646e76bdcf17b7b
saiKelkar <104817425+saiKelkar@users.noreply.github.com>
2023-05-27 11:34:38 +0530

    ps0.py

---
+++ ps0.py
@@ -0,0 +1,11 @@
+# Problem set 0
+# Name: Sai Kelkar
+# Time: 09.00 pm
+
+import math
+
+num1 = int(input("Enter a number x: "))
+num2 = int(input("Enter a number y: "))
+
+print("x**y: ", num1**num2)
+print("log(x): ", math.log(num1, 2))
